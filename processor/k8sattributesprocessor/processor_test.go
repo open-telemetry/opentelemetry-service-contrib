0f6042225ed197e3845d35fc36ca0ce279078da0
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-01-09 12:33:51 +0100

    add service version

--- processor/k8sattributesprocessor/processor_test.go
+++ processor/k8sattributesprocessor/processor_test.go
@@ -1038,6 +1038,7 @@ func TestProcessorAddContainerAttributes(t *testing.T) {
 								ImageName:         "test/app",
 								ImageTag:          "1.0.1",
 								ServiceInstanceID: "instance-1",
+								ServiceVersion:    "1.0.1",
 							},
 						},
 					},
@@ -1053,6 +1054,7 @@ func TestProcessorAddContainerAttributes(t *testing.T) {
 				conventions.AttributeContainerImageName: "test/app",
 				conventions.AttributeContainerImageTag:  "1.0.1",
 				conventions.AttributeServiceInstanceID:  "instance-1",
+				conventions.AttributeServiceVersion:     "1.0.1",
 			},
 		},
 		{
@@ -1093,6 +1095,8 @@ func TestProcessorAddContainerAttributes(t *testing.T) {
 				conventions.AttributeContainerImageName: "test/app",
 				conventions.AttributeContainerImageTag:  "1.0.1",
 				conventions.AttributeServiceInstanceID:  "instance-1",
+				conventions.AttributeServiceVersion:     "1.0.1",
+				conventions.AttributeServiceName:        "app",
 			},
 		},
 		{
@@ -1112,6 +1116,8 @@ func TestProcessorAddContainerAttributes(t *testing.T) {
 				kp.kc.(*fakeClient).Pods[newPodIdentifier("resource_attribute", "k8s.pod.uid", "19f651bc-73e4-410f-b3e9-f0241679d3b8")] = &kube.Pod{
 					Attributes: map[string]string{
 						"service.instance.id": "explicit-1",
+						"service.version":     "1.0.2",
+						"service.name":        "test-app",
 					},
 					Containers: kube.PodContainers{
 						ByID: map[string]*kube.Container{
@@ -1136,6 +1142,8 @@ func TestProcessorAddContainerAttributes(t *testing.T) {
 				conventions.AttributeContainerImageName: "test/app",
 				conventions.AttributeContainerImageTag:  "1.0.1",
 				conventions.AttributeServiceInstanceID:  "explicit-1",
+				conventions.AttributeServiceVersion:     "1.0.2",
+				conventions.AttributeServiceName:        "test-app",
 			},
 		},
 		{
