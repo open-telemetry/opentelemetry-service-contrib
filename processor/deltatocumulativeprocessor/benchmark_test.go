f67a6034c2661a08ed0b46d3bc9b3b103691c877
sh0rez <me@shorez.de>
2025-01-30 12:50:23 +0100

    processor/deltatocumulative: parallel benchmark

--- processor/deltatocumulativeprocessor/benchmark_test.go
+++ processor/deltatocumulativeprocessor/benchmark_test.go
@@ -5,12 +5,17 @@ package deltatocumulativeprocessor
 
 import (
 	"context"
+	"fmt"
 	"math/rand/v2"
+	"runtime"
 	"strconv"
+	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
@@ -19,8 +24,6 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/deltatocumulativeprocessor/internal/data/expo/expotest"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/deltatocumulativeprocessor/internal/data/histo"
 )
-
-var out *consumertest.MetricsSink
 
 func BenchmarkProcessor(gb *testing.B) {
 	const (
@@ -124,8 +127,8 @@ func BenchmarkProcessor(gb *testing.B) {
 
 	for _, cs := range cases {
 		gb.Run(cs.name, func(b *testing.B) {
-			st := setup(b, nil)
-			out = st.sink
+			sink := new(consumertest.MetricsSink)
+			proc, _ := setup(b, nil, sink)
 
 			md := pmetric.NewMetrics()
 			ms := md.ResourceMetrics().AppendEmpty().ScopeMetrics().AppendEmpty().Metrics()
@@ -148,14 +151,95 @@ func BenchmarkProcessor(gb *testing.B) {
 				md.CopyTo(req)
 
 				b.StartTimer()
-				err := st.proc.ConsumeMetrics(ctx, req)
+				err := proc.ConsumeMetrics(ctx, req)
 				b.StopTimer()
 				require.NoError(b, err)
 			}
 
 			// verify all dps are processed without error
 			b.StopTimer()
-			require.Equal(b, b.N*metrics*streams, st.sink.DataPointCount())
+			require.Equal(b, b.N*metrics*streams, sink.DataPointCount())
 		})
 	}
+}
+
+func Benchmark(b *testing.B) {
+	const K = 64               // number of metrics
+	const S = 3                // number of streams
+	P := runtime.GOMAXPROCS(0) // number of routines
+	if P > K {
+		b.Fatal("increase K")
+	}
+
+	var id atomic.Int64
+	id.Store(-1)
+
+	var init sync.WaitGroup
+	init.Add(P)
+	wait := make(chan struct{})
+
+	start := time.Now()
+
+	sink := new(CountingSink)
+	proc, _ := setup(b, nil, sink)
+
+	b.ReportAllocs()
+	go func() {
+		init.Wait()
+		b.ResetTimer()
+		close(wait)
+	}()
+
+	k := K / P
+	b.RunParallel(func(pb *testing.PB) {
+		md := pmetric.NewMetrics()
+		ms := make([]pmetric.Metric, k)
+		for i := range ms {
+			m := md.ResourceMetrics().AppendEmpty().ScopeMetrics().AppendEmpty().Metrics().AppendEmpty()
+			mid := id.Add(1)
+			m.SetName(fmt.Sprintf("metric-%d", mid))
+			sum := m.SetEmptySum()
+			for s := range S {
+				dp := sum.DataPoints().AppendEmpty()
+				dp.Attributes().PutInt("s", int64(s))
+				dp.SetIntValue(rand.Int64N(100))
+				dp.SetStartTimestamp(pcommon.NewTimestampFromTime(start))
+			}
+			ms[i] = m
+		}
+		ctx := context.Background()
+		init.Done()
+
+		<-wait
+		for n := 0; pb.Next(); n++ {
+			for _, m := range ms {
+				m.Sum().SetAggregationTemporality(pmetric.AggregationTemporalityDelta)
+				dps := m.Sum().DataPoints()
+				for i := range dps.Len() {
+					dp := dps.At(i)
+					dp.SetTimestamp(dp.Timestamp() + pcommon.Timestamp(time.Minute.Nanoseconds()))
+				}
+			}
+			if err := proc.ConsumeMetrics(ctx, md); err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+	b.StopTimer()
+
+	dps := K / P * S * b.N
+	require.Equal(b, int64(dps), sink.Load())
+}
+
+type CountingSink struct {
+	atomic.Int64
+}
+
+func (cs *CountingSink) ConsumeMetrics(_ context.Context, md pmetric.Metrics) error {
+	cs.Add(int64(md.DataPointCount()))
+	return nil
+}
+
+func (cs *CountingSink) Capabilities() consumer.Capabilities {
+	return consumer.Capabilities{}
 }
